73af7bb5c06dffaf7a06e4a4c9b8e959028f5d24
pixxxelolo <pixxxelolo@gmail.com>
2016-10-10 22:36:16 +0300

    add Statistics

---
+++ public/javascripts/classes/Statistics.js
@@ -0,0 +1,32 @@
+define([], function(){
+
+	var statistics = {
+		rightAnswers_count: 0,
+		mistakes_count: 0
+	}
+
+	statistics.resetStatistic = function(){
+		statistics.rightAnswers_count = 0;
+		statistics.mistakes_count = 0;
+	}
+
+	statistics.incRightAnswers_count = function(){
+		statistics.rightAnswers_count++;
+	}
+
+	statistics.incMistakes_count = function(){
+		statistics.mistakes_count++;
+	}
+
+	statistics.getStatistics = function(){
+		var obj = {
+			rightAnswers_count: statistics.rightAnswers_count,
+			mistakes_count: statistics.mistakes_count
+		}
+
+		console.log(obj);
+		return obj;
+	}
+
+	return statistics;
+})
